48d68fb316ab0a3367d529e8d0a61bd0e5e9be9d
Danny Guzman <guzmanjd86@gmail.com>
2023-12-16 06:24:20 -0800

    Removed more Divi references

--- tag.php
+++ tag.php
@@ -22,10 +22,8 @@ if ( have_posts() ) :
 		?>
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<a class="thumbnail-link no-underline" href="<?php the_permalink(); ?>">
-					<?php
+				<?php
 					if ( has_post_thumbnail() ) {
-						$caweb_thumb_id  = get_post_thumbnail_id( get_the_ID() );
-						$caweb_thumb_alt = get_post_meta( $caweb_thumb_id, 'wp_attachment_image_alt', true );
 						the_post_thumbnail(
 							'medium',
 							array(
@@ -36,26 +34,18 @@ if ( have_posts() ) :
 					?>
 					<span class="sr-only">Read more about <?php the_title(); ?></span>
 				</a>
-				<?php
-				if ( function_exists( 'et_divi_post_format_content' ) ) {
-					et_divi_post_format_content();
-				}
-				?>
+				
 				<div class="tag-info">
 					<a class="title" href="<?php the_permalink(); ?>">
 						<h2><?php ( ! empty( the_title( '', '', false ) ) ? the_title() : print 'No Title' ); ?></h2>
 					</a>
 					<?php
-					if ( function_exists( 'et_divi_post_meta' ) ) {
-						et_divi_post_meta();
-					}
+						caweb_post_meta();
 					?>
 				</div>
 				<p>
 					<?php
-					if ( function_exists( 'truncate_post' ) ) {
-						truncate_post( 270 );
-					}
+					the_excerpt();
 					?>
 					<a class="btn btn-default" href="<?php the_permalink(); ?>">Read More<span class="sr-only">Read more about <?php the_title(); ?></span></a>
 				</p>
@@ -64,12 +54,10 @@ if ( have_posts() ) :
 		endwhile;
 	?>
 	<div class="pagination clearfix">
-		<div class="alignleft"><?php next_posts_link( esc_html__( '&laquo; Older Entries', 'Divi' ) ); ?></div>
-		<div class="alignright"><?php previous_posts_link( esc_html__( 'Next Entries &raquo;', 'Divi' ) ); ?></div>
+		<div class="pull-left"><?php next_posts_link(esc_html('&laquo; Older Entries')); ?></div>
+		<div class="pull-right"><?php previous_posts_link(esc_html('Next Entries &raquo;')); ?></div>
 	</div>
 	<?php
-else :
-	get_template_part( 'includes/no-results', 'index' );
 endif;
 
 
